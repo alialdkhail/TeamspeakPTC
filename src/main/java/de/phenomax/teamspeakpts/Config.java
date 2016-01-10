0e11273d6d3f765e246144cc82942cfa7c544e26
Phenomax <phenomax@minora.io>
2016-01-10 17:02:54 +0100

    remove unnecessary files

--- src/main/java/de/phenomax/teamspeakpts/Config.java
+++
@@ -1,7 +0,0 @@
-package de.phenomax.teamspeakpts;
-
-/**
- * Created by phenomax on 10.1.2015
- */
-public class Config {
-}
--- src/main/java/de/phenomax/teamspeakpts/data/UserData.java
+++
@@ -1,7 +0,0 @@
-package de.phenomax.teamspeakpts.data;
-
-/**
- * Created by phenomax on 10.1.2015
- */
-public class UserData {
-}
